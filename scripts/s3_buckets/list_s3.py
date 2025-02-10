b010e5f05043e1aae0363d2f764799a9ef82212e
gliecco <juanlagospg@gmail.com>
2025-02-10 11:27:39 -0300

    enhancement: add input validation for the s3 buckets listing script

--- scripts/s3_buckets/list_s3.py
+++ scripts/s3_buckets/list_s3.py
@@ -7,10 +7,12 @@ load_dotenv()
 s3 = boto3.client('s3', endpoint_url=os.getenv('ENDPOINT_URL'))
 
 def list_s3_buckets():
-    response = s3.list_buckets()
-
-    print('Buckets do S3 existentes:')
-    for bucket in response['Buckets']:
-        print(f'  {bucket["Name"]}')
+    buckets_list = s3.list_buckets()
     
-list_s3_buckets()
+    if not buckets_list['Buckets']:
+        print('Não há buckets do S3 para listar.')
+    else:    
+        print('\nBuckets do S3 existentes:')
+        for bucket in buckets_list['Buckets']:
+            print(f'  Nome: {bucket["Name"]}')
+    return buckets_list
