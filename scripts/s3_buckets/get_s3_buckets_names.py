4d6dc9ea0c3e63cb7e04b12a3e9d4d1ac84fd753
gliecco <juanlagospg@gmail.com>
2025-02-11 12:30:11 -0300

    feat: add a get_s3_bucket_names that returns the s3 bucket names as a list

---
+++ scripts/s3_buckets/get_s3_buckets_names.py
@@ -0,0 +1,22 @@
+import os
+import boto3
+from dotenv import load_dotenv
+
+load_dotenv()
+
+s3 = boto3.client('s3', endpoint_url=os.getenv('ENDPOINT_URL'))
+
+def get_s3_bucket_names():
+    buckets_list = s3.list_buckets()
+    no_buckets = 'Não há buckets do S3 para listar.'
+    bucket_names = [] 
+
+    if not buckets_list['Buckets']:
+        return no_buckets
+    else:  
+        for bucket in buckets_list['Buckets']:
+            bucket_names.append({bucket["Name"]})
+        return bucket_names
+
+if __name__ == "__main__":  
+    get_s3_bucket_names() 
