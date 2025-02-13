9e7207c5baf449f7d79e4094f9ecb93625ec37c9
gliecco <juanlagospg@gmail.com>
2025-02-13 17:02:58 -0300

    feat: add delete s3 buckets screen

---
+++ ui/screens/delete_s3_buckets.py
@@ -0,0 +1,21 @@
+from textual.app import ComposeResult
+from textual.containers import Center
+from textual.widgets import Header, Button
+from textual.screen import Screen
+import yaml
+
+with open("resources/ptbr.yaml", "r") as f:
+    ptbr = yaml.safe_load(f)
+
+class S3BucketsDeleteScreen(Screen):
+    def __init__(self, bucket_names: list):
+        super().__init__()
+        self.bucket_names = bucket_names
+
+    def compose(self) -> ComposeResult:
+            yield Header()
+            with Center():
+                for bucket in self.bucket_names:
+                    yield Button(f'Nome: {bucket}')
+                yield Button(ptbr['button']['back'], id='back-btn')
+                yield Button(ptbr['button']['reload'], id='reload-btn')
