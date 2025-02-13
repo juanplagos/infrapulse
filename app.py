73a83cfecde87f6f8f0f54a8c6ae8596170af3c7
gliecco <juanlagospg@gmail.com>
2025-02-13 17:01:54 -0300

    refactor: separate bucket list validation logic from button click event handling on main page

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from textual.app import App
 from textual import on
+from textual.app import App
 from textual.widgets import Button
-from src.scripts.s3_buckets.get_s3_buckets_names import get_s3_bucket_names
 from ui.screens.home import HomeScreen
 from ui.screens.s3_buckets_list import S3BucketsListScreen
+from ui.screens.delete_s3_buckets import S3BucketsDeleteScreen
+from utils.validate_s3_buckets_list import list_check
+from utils.reload_page import reload_page
+import yaml
+
+with open("resources/ptbr.yaml", "r") as f:
+    ptbr = yaml.safe_load(f)
 
 class Infrapulse(App):
     CSS_PATH = 'resources/styles.tcss'
@@ -13,28 +19,29 @@ class Infrapulse(App):
         super().__init__()
         self.bucket_names = [] 
 
-    async def on_mount(self) -> None:
-        await self.push_screen(HomeScreen())
+    def on_mount(self) -> None:
+        self.push_screen(HomeScreen())
 
     @on(Button.Pressed, '#list-s3-btn')
     async def list_s3_buckets(self) -> None:
-            self.notify('Listando Buckets do S3...', timeout=2.0)
-        
-            names = get_s3_bucket_names()
-                
-            if isinstance(names, list):
-                self.bucket_names = names 
-                await self.push_screen(S3BucketsListScreen(self.bucket_names))
-            else:
-                self.notify('Erro ao listar buckets', severity='error', timeout=3.0)
+        self.notify(ptbr['notification']['listing'], timeout=2.0)
+        await list_check(self, S3BucketsListScreen(self.bucket_names))
+
+    @on(Button.Pressed, '#delete-s3-btn')
+    async def delete_s3_buckets(self) -> None:
+        await list_check(self, S3BucketsDeleteScreen(self.bucket_names))
 
     @on(Button.Pressed, '#back-btn')
-    async def navigate_back(self):
-            await self.pop_screen()
+    async def navigate_back(self) -> None:
+        self.pop_screen()
+    
+    @on(Button.Pressed, '#reload-btn')
+    def refresh_page(self) -> None:
+        reload_page(self)
 
     @on(Button.Pressed, '#exit-btn')
-    def exit_the_app(self):
-            self.exit()
+    def exit_the_app(self) -> None:
+        self.exit()
 
 if __name__ == "__main__":
     app = Infrapulse()
