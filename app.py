82ccd9dbfd1acba929f1723b9e675cf04c858933
gliecco <juanlagospg@gmail.com>
2025-02-13 14:37:16 -0300

    fix: use the @on decorator to handle events

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from textual.app import App
+from textual import on
 from textual.widgets import Button
 from src.scripts.s3_buckets.get_s3_buckets_names import get_s3_bucket_names
 from ui.screens.home import HomeScreen
@@ -15,27 +16,25 @@ class Infrapulse(App):
     async def on_mount(self) -> None:
         await self.push_screen(HomeScreen())
 
-    async def on_button_pressed(self, event: Button.Pressed) -> None:
-        match event.button.id:
-            case 'list-s3-btn':
-                self.notify('Listando Buckets do S3...', timeout=2.0)
+    @on(Button.Pressed, '#list-s3-btn')
+    async def list_s3_buckets(self) -> None:
+            self.notify('Listando Buckets do S3...', timeout=2.0)
         
-                names = get_s3_bucket_names()
+            names = get_s3_bucket_names()
                 
-                if isinstance(names, list):
-                    self.bucket_names = names 
-                    await self.push_screen(S3BucketsListScreen(self.bucket_names))
-                else:
-                    self.notify('Erro ao listar buckets', severity='error', timeout=3.0)
+            if isinstance(names, list):
+                self.bucket_names = names 
+                await self.push_screen(S3BucketsListScreen(self.bucket_names))
+            else:
+                self.notify('Erro ao listar buckets', severity='error', timeout=3.0)
 
-            case 'back-btn':
-                await self.pop_screen()
+    @on(Button.Pressed, '#back-btn')
+    async def navigate_back(self):
+            await self.pop_screen()
 
-            case 'delete-s3-btn':
-                pass
-
-            case 'exit-btn':
-                self.exit()
+    @on(Button.Pressed, '#exit-btn')
+    def exit_the_app(self):
+            self.exit()
 
 if __name__ == "__main__":
     app = Infrapulse()
