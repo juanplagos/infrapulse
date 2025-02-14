11fab49c48649d635edb0c2ea6baed52fe5234d5
gliecco <juanlagospg@gmail.com>
2025-02-14 15:22:26 -0300

    fix: fix reload_page function to reload correct screen

--- utils/reload_page.py
+++ utils/reload_page.py
@@ -1,10 +1,13 @@
+from typing import Callable
 import yaml
+
 
 with open("resources/ptbr.yaml", "r") as f:
     ptbr = yaml.safe_load(f)
 
-def reload_page(self) -> None:
+def reload_page(self, screen_name: Callable) -> None:
     self.notify(ptbr['notification']['reloading'], timeout=2.0)
+    self.push_screen(screen_name)
 
 if __name__ == '__main__':
     reload_page()
