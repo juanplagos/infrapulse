1fdde5f6fad88283e448c14a82655bd1961122f6
gliecco <juanlagospg@gmail.com>
2025-02-21 09:40:20 -0300

    fix: push buckets list screen regardless if buckets are available or not

--- utils/validate_buckets_list.py
+++ utils/validate_buckets_list.py
@@ -1,7 +1,7 @@
 from src.scripts.s3_buckets.get_s3_buckets_names import get_s3_bucket_names
 from typing import Callable
 
-async def list_check(self, screen_name: Callable):
+async def list_check(self, screen_name: Callable) -> None:
     names = get_s3_bucket_names()
 
     if names:
@@ -9,6 +9,7 @@ async def list_check(self, screen_name: Callable):
         self.push_screen(screen_name)
     else:
         self.notify('Erro ao listar buckets', severity='error', timeout=3.0)
+        self.push_screen(screen_name)
 
 if __name__ == '__main__':
     list_check()
