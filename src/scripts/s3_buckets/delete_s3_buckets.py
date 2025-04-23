d19ced8d50ca7721f31ad37de8253a34ae443eda
gliecco <juanlagospg@gmail.com>
2025-04-23 11:42:41 -0300

    feat: add error handling

--- src/scripts/s3_buckets/delete_s3_buckets.py
+++ src/scripts/s3_buckets/delete_s3_buckets.py
@@ -7,12 +7,17 @@ load_dotenv()
 
 s3 = boto3.client('s3', endpoint_url=os.getenv('ENDPOINT_URL'))
 
+# Deletes empty s3 buckets
 def delete_s3_buckets(self) -> None:
     self.users_bucket_name = self.query_one('#user_bucket_name').value
     
     self.notify(f'Deletando bucket {self.users_bucket_name}...', timeout=2.0)
-    s3.delete_bucket(Bucket=self.users_bucket_name)
-    self.notify(f'Bucket {self.users_bucket_name} deletado com sucesso.')
+
+    try:
+        s3.delete_bucket(Bucket=self.users_bucket_name)
+        self.notify(f'Bucket {self.users_bucket_name} deletado com sucesso.')
+    except Exception as e:
+        self.notify(f'Erro ao deletar bucket: {e}', severity='error', timeout=5.0)
 
 if __name__ == "__main__":
     delete_s3_buckets()  
