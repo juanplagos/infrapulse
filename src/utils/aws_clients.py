0892803c97453213567daeb9a92cd3602927ed6e
gliecco <juanlagospg@gmail.com>
2025-04-25 11:56:13 -0300

    fix: update get_client type hinting

--- src/utils/aws_clients.py
+++ src/utils/aws_clients.py
@@ -4,5 +4,5 @@ import boto3
 
 load_dotenv()
 
-def get_client(service_name: str) -> None:
+def get_client(service_name: str):
     return boto3.client(service_name, endpoint_url=os.getenv('ENDPOINT_URL'))
